19f51fe18972a249408fe245584c3d5a3673a7cb
Aura Tao <auratao.model@gmail.com>
2024-08-16 09:08:36 -0400

    naming

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/week_fourth_vite/",
+  base: "/week_fourth_challenge/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
